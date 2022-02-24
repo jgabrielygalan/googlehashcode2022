0259224199cfbe5330c6b878421bfafee6212224
Edgaras Makarovas <edgaras.makarovas@docomodigital.com>
2022-02-24 19:14:16 +0100

    File reading

--- src/main/java/jesus/gabriel/hashcode/files/FileReader.java
+++ src/main/java/jesus/gabriel/hashcode/files/FileReader.java
@@ -2,34 +2,64 @@ package jesus.gabriel.hashcode.files;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import jesus.gabriel.hashcode.photo.Direction;
+import jesus.gabriel.hashcode.model.Contributor;
+import jesus.gabriel.hashcode.model.Project;
 import jesus.gabriel.hashcode.photo.Photo;
 
 public class FileReader {
 
 	public Set<Photo> parseInputFile(File f) {
+		final Set<Contributor> contributors = new HashSet<>();
+		final Set<Project> projects = new HashSet<>();
 		try {
-			final Set<Photo> returnSet = new HashSet<Photo>();
-			final Scanner sc = new Scanner(f);
-			final int numberOfPhotos = sc.nextInt();
-			for(int id=0; id<numberOfPhotos; id++) {
-				final Direction direction = Direction.getByLetter(sc.next());
-				final int numberOfTags = sc.nextInt();
-				final Set<String> tags = new HashSet<String>(2);
-				for(int j=0; j<numberOfTags; j++) {
-					tags.add(sc.next());
-				}
-				returnSet.add(new Photo(id, direction, tags));
-			}
-			sc.close();
-			return returnSet;
-		} catch (FileNotFoundException e) {
-			// ignore
-			throw new RuntimeException(e);
-		}
+		  final Scanner sc = new Scanner(f);
+		  final int numberOfContributors = sc.nextInt();
+		  final int numberOfProjects = sc.nextInt();
+          for(int id=0; id<numberOfContributors; id++) {
+            final String contributorName = sc.next();
+            final int numberOfSkills = sc.nextInt();
+            final Map<String, Integer> skills = new HashMap<>();
+            for (int skillId = 0; skillId<numberOfSkills; skillId++) {
+              final String skillName = sc.next();
+              final int skillLevel = sc.nextInt();
+              skills.put(skillName, skillLevel);
+            }
+            final Contributor contributor = new Contributor();
+            contributor.setName(contributorName);
+            contributor.setSkills(skills);
+            contributors.add(contributor);
+          }
+          for(int id=0; id<numberOfProjects; id++) {
+            final String projectName = sc.next();
+            final int numberOfDaysToComplete = sc.nextInt();
+            final int projectScore = sc.nextInt();
+            final int bestBefore = sc.nextInt();
+            final int numberOfRoles = sc.nextInt();
+            final Project project = new Project();
+            project.setName(projectName);
+            project.setBestBefore(bestBefore);
+            project.setLength(numberOfDaysToComplete);
+            project.setScore(projectScore);
+            final Map<String, Integer> roles = new HashMap<>();
+            project.setRoles(roles);
+            for(int roleId=0; roleId<numberOfRoles; roleId++) {
+              final String skillName = sc.next();
+              final int skillLevel = sc.nextInt();
+              roles.put(skillName, skillLevel);
+            }
+            projects.add(project);
+          }
+          sc.close();
+          return returnSet;
+      } catch (FileNotFoundException e) {
+          // ignore
+          throw new RuntimeException(e);
+      }
 	}
 	
 }
